0599100741fa20100ba6b125b4630a6620a2afe9
Roman Kochnev <romaslog@icloud.com>
2023-07-07 20:52:32 +0500

    Rename

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void SumOfAllSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedDay = 9;
+        int actualDay = service.minSales(sales);
+
+        Assertions.assertEquals(expectedDay, actualDay);
+
+    }
+}
+
